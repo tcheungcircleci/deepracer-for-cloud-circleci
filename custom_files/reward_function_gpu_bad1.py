bd8580cb2e5e7ab360ce69d73eb4b1d32f4b71e2
Tim Cheung <tim.cheung@circleci.com>
2022-05-31 10:27:17 -0400

    edit

--- custom_files/reward_function_gpu_bad1.py
+++ custom_files/reward_function_gpu_bad1.py
@@ -1,3 +1,5 @@
+import math
+
 def reward_function(params):
     '''
     Example of penalize steering, which helps mitigate zig-zag behaviors
@@ -7,27 +9,32 @@ def reward_function(params):
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
     steering = abs(params['steering_angle']) # Only need the absolute steering angle
-
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    progress = params['progress']
+    all_wheels_on_track = params['all_wheels_on_track']
+    is_offtrack = params['is_offtrack']
+    speed = params['speed']
 
     # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+    # +1 at center, -1 at edge
+    reward = math.cos( 2 * (distance_from_center/track_width) * math.pi)
+
+    # Higher reward for higher speed
+    reward += (speed/4)-0.25
 
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 15
 
     # Penalize reward if the car is steering too much
     if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+        reward -= 0.5
+
+    if progress in [25,50,75,100]:
+        reward += progress
+
+    if not all_wheels_on_track:
+        reward = -5
+
+    if is_offtrack:
+        reward = -100
 
     return float(reward)
